3604278aa1d883c7afb8defd4193ad39ae2f9f8a
Amina-Remadna <aminaremadna6@gmail.com>
2023-06-19 10:18:49 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,11 @@
  * This program prints a string using the puts function.
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-    	return (0);
+	return (0);
+
 }
