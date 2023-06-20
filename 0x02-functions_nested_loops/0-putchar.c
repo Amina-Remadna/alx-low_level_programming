da5770011e876978b1e733880eaded14c4c910da
Amina-Remadna <aminaremadna6@gmail.com>
2023-06-20 08:55:49 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* main - entry poin
+*
+* Description: print _putchar using putchar prototype
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "_putchar";
+	int chi;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar(`\n`);
+
+	return (0);
+}
