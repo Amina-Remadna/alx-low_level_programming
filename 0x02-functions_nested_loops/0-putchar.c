6d0845ed0ff2211847138c42351ac0b7f8e79247
Amina-Remadna <aminaremadna6@gmail.com>
2023-06-20 11:49:45 -0700

    a

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,15 +6,16 @@
 *
 * Return: Always 0 (Success)
 */
-
-int main(void)
+intmain(void)
 {
-	char str[] = "_putchar";
-	int chi;
-
-	for (ch = 0; ch < 8; ch++)
-		_putchar(str[ch]);
-	_putchar(`\n`);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
