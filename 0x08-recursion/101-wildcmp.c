18d4160168b55e64eb3e0498a975eee5c03ed67b
amina <amina@gmail.com>
2023-07-30 10:33:16 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * move_past_start - iterates past asterisk
+ * move_past_star - iterates past asterisk
  * @s2: the second string, can contain wildcard
  *
  * Return: the pointer past star
@@ -43,7 +43,7 @@ int wildcmp(char *s1, char *s2)
 	int ret = 0;
 
 
-	if (!*s1 && *s2 == '* && !*move_past_star(s2))
+	if (!*s1 && *s2 == '*' && !*move_past_star(s2))
 		return (1);
 	if (*s1 == *s2)
 	{
